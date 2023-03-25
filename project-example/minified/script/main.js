0e084d80b5ee501ac57ae48be871de20fcc0d68c
pabcrudel <pabcrudel@gmail.com>
2023-03-25 17:24:15 +0100

    Creado ejemplo de proyecto minificado

---
+++ project-example/minified/script/main.js
@@ -0,0 +1,1 @@
+document.getElementById("myText").innerHTML="Texto procedente de JavaScript";
---
+++ project-example/script/main.js
@@ -0,0 +1,2 @@
+// Texto que se va a pintar en el HTML con javascript
+document.getElementById("myText").innerHTML = "Texto procedente de JavaScript";
